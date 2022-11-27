16c9824f2290320c16a8b9771016c00233ba5bad
Jose <keivys17@gmail.com>
2022-11-27 13:23:54 -0500

    Se agrega hoja de vida en HTML y CSS

---
+++ PerfilFront/app.js
@@ -0,0 +1,19 @@
+var acc = document.getElementsByClassName("accordion");
+var acc1 = document.getElementsByClassName("infoContainer");
+var i;
+
+
+function Main() {
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+}
